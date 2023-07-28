9b5113605fa86c09b31ea5ec4c6a45835bb5a12f
Nickolay <xenothorn@yandex.ru>
2023-07-28 03:36:20 +0300

    Не работает Resize со строками (возможно с интами тоже)

--- main.cpp
+++ main.cpp
@@ -1,68 +1,40 @@
-#include <cassert>
+#include "simple_vector.h"
 #include <iostream>
-#include <string_view>
-#include <vector>
-#include <list>
-#include <map>
-
-using namespace std;
-
-class Translator {
-public:
-    Translator() = default;
-
-    void Add(string_view source, string_view target)
-    {
-       fbase_.emplace(make_pair(source,target));
-       bbase_.emplace(make_pair(target,source));
-    }
-
-
-    string_view TranslateForward(string_view source) const
-    {
-        using namespace std::literals;
-        string_view s = ""sv;
-        if (fbase_.count(static_cast<string>(source)))
-        {
-            return fbase_.at(static_cast<string>(source));
-        }
-        else
-        {
-            return s;
-        }
-    }
-
-    string_view TranslateBackward(string_view target) const
-    {
-        using namespace std::literals;
-        string_view s = ""sv;
-        if (bbase_.count(static_cast<string>(target)))
-        {
-            return bbase_.at(static_cast<string>(target));
-        }
-        else
-        {
-            return s;
-        }
-    }
-
-private:
-    map<string, string> fbase_;
-    map<string, string> bbase_;
-};
-
-
-void TestSimple() {
-    Translator translator;
-    translator.Add(string("okno"s), string("window"s));
-    translator.Add(string("stol"s), string("table"s));
-
-    assert(translator.TranslateForward("okno"s) == "window"s);
-    assert(translator.TranslateBackward("table"s) == "stol"s);
-    assert(translator.TranslateForward("table"s) == ""s);
-}
+// Tests
+#include "tests.h"
 
 int main() {
-    TestSimple();
+ //   Test1();
+
+    SimpleVector v{1,2,3,4,5,6};
+    SimpleVector s{"hell", "bell", "tell"};
+
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << s[i] << ' ';
+    }std::cout << std::endl;
+
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << s.At(i) << ' ';
+    }std::cout << std::endl;
+
+    for (auto it = s.begin(); it != s.end(); it++)
+    {
+        std::cout << *it << ' ';
+    }std::cout << std::endl;
+
+    s.Resize(10);
+    
+    for (auto it = s.begin(); it != s.end(); it++)
+    {
+        std::cout << *it << ' ';
+    }std::cout << std::endl;    
+
+
+    std::cout << " -- OK --- " << std::endl;
+
+
+
     return 0;
-} 
+}
---
+++ simple_vector.h
@@ -0,0 +1,204 @@
+#pragma once
+
+#include <cassert>
+#include <initializer_list>
+#include <stddef.h>
+#include <stdexcept>
+
+#include <iostream>
+
+
+
+template <typename Type>
+class SimpleVector {
+public:
+    using Iterator = Type*;
+    using ConstIterator = const Type*;
+
+    SimpleVector() noexcept = default;
+ 
+    // Создаёт вектор из size элементов, инициализированных значением по умолчанию
+    explicit SimpleVector(size_t size) {
+        // Напишите тело конструктора самостоятельно
+        SimpleVector tmp;
+        tmp.items_ = new Type[size];
+        tmp.capacity_ = size;
+        tmp.size_ = size;
+        swap(tmp);
+    }
+
+    // Создаёт вектор из size элементов, инициализированных значением value
+    SimpleVector(size_t size, const Type& value) {
+        // Напишите тело конструктора самостоятельно
+        SimpleVector tmp;
+        tmp.items_ = new Type[size];
+        tmp.capacity_ = size;
+        tmp.size_ = size;
+        std::fill(tmp.begin(), tmp.end(), value);
+        swap(tmp);
+    }
+
+    // Создаёт вектор из std::initializer_list
+    SimpleVector(std::initializer_list<Type> init) {
+        SimpleVector tmp(init.size());
+        int i = 0;
+        for (auto it = init.begin(); it != init.end(); ++it)
+        {
+            tmp[i++] = *it;
+        }
+        swap(tmp);
+    }
+
+    ~SimpleVector()
+    {
+        delete [] items_;
+    }
+
+    // Возвращает количество элементов в массиве
+    size_t GetSize() const noexcept {
+        return size_;
+    }
+
+    // Возвращает вместимость массива
+    size_t GetCapacity() const noexcept {
+        return capacity_;
+    }
+
+    // Сообщает, пустой ли массив
+    bool IsEmpty() const noexcept {
+        return (size_ == 0);
+    }
+
+    // Возвращает ссылку на элемент с индексом index
+    Type& operator[](size_t index) noexcept {
+        return items_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    const Type& operator[](size_t index) const noexcept {
+        return items_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    // Выбрасывает исключение std::out_of_range, если index >= size
+    Type& At(size_t index) {
+        if (index >= size_) throw std::out_of_range("out vector range");
+        else
+            return (items_[index]);
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    // Выбрасывает исключение std::out_of_range, если index >= size
+    const Type& At(size_t index) const {
+        if (index >= size_) throw std::out_of_range("out vector range");
+        else
+            return (items_[index]);
+    }
+
+    // Обнуляет размер массива, не изменяя его вместимость
+    void Clear() noexcept {
+        size_ = 0;
+    }
+
+    // Изменяет размер массива.
+    // При увеличении размера новые элементы получают значение по умолчанию для типа Type
+    void Resize(size_t new_size) {
+        // Напишите тело самостоятельно
+        if (new_size <= size_) size_ = new_size;
+        else if (new_size <= capacity_)
+        {
+            std::cout << "tut" <<std::endl;
+            auto it = end();
+            size_ = new_size;
+            std::fill(it, end(), Type{});
+        }
+        else
+        {
+            SimpleVector tmp(new_size);
+            //std::fill(tmp.begin(), tmp.end(), Type{});
+            std::copy(begin(), end(), tmp.begin());
+
+            std::cout <<"before swap : ";
+            for (auto it = tmp.begin(); it < tmp.end(); it++)
+            {
+                std::cout << *it << ' ';
+            }std::cout << "test" << std::endl;
+
+            std::cout << tmp.items_ << " --- " << items_ << std::endl;
+            std::cout << tmp.size_ << " --- " << size_ << std::endl;
+            std::cout << tmp.capacity_ << " --- " << capacity_ << std::endl;
+
+
+
+
+
+
+            std::cout <<"right after swap : ";
+            for (auto it = begin(); it != end(); it++)
+            {
+                std::cout << *it << ' ';
+            }std::cout << std::endl;
+
+        }
+    }
+
+
+    void swap(SimpleVector& v) noexcept {
+        std::swap(v.items_, items_);
+        std::swap(v.size_, size_);
+        std::swap(v.capacity_, capacity_);
+    }
+
+    // Возвращает итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    Iterator begin() noexcept {
+        // Напишите тело самостоятельно
+        if (size_ == 0) return nullptr;
+        else return items_;
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    Iterator end() noexcept {
+        if (size_ == 0) return nullptr;
+        else return (items_ + size_);
+    }
+
+    // Возвращает константный итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator begin() const noexcept {
+        if (size_ == 0) return nullptr;
+        else return items_;
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator end() const noexcept {
+        if (size_ == 0) return nullptr;
+        else return (items_ + size_);
+    }
+
+    // Возвращает константный итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator cbegin() const noexcept {
+        return begin();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator cend() const noexcept {
+        return end();
+    }
+
+
+
+private:
+    Type* items_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+};
+
+template <typename T>
+void swap(SimpleVector<T> v, SimpleVector<T> t) noexcept {
+    std::swap(v, t);
+}
